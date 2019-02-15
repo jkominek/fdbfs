2a535cc26b90b9616b8bf86fbaee9411ce846140
Jay Kominek <kominek@gmail.com>
2019-02-15 00:00:00 -0700

    introduce debug_print

--- util.h
+++ util.h
@@ -30,4 +30,11 @@ extern void pack_inode_key(fuse_ino_t ino, uint8_t *key, int *keylen);
 extern void pack_dentry_key(fuse_ino_t ino, char *name, int namelen, uint8_t *key, int *keylen);
 extern void unpack_stat_from_dbvalue(uint8_t *val, int vallen, struct stat *attr);
 
+#ifndef DEBUG
+#define DEBUG 0
+#endif //DEBUG
+
+#define debug_print(fmt, ...) \
+  do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
+
 #endif // __UTIL_H_
