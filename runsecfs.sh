2ba89cfad6b4a68644dd64551e2216b8a33801af
Jay Kominek <kominek@gmail.com>
2021-12-22 21:41:19 -0700

    adding some sleep

--- runsecfs.sh
+++ runsecfs.sh
@@ -4,6 +4,10 @@ rm -rf /tmp/fdb
 mkdir /tmp/fdb
 valgrind --log-file=valgrind.txt ./fs -o default_permissions,allow_other /tmp/fdb &
 
+echo sleeping for a bit, because of slowness
+sleep 20
+echo resuming...
+
 cd /tmp
 git clone https://github.com/billziss-gh/secfs.test
 cd /tmp/secfs.test/fstest/fstest
